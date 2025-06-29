62e36f0a2de7b9af9b952b5232c3ff987c701b06
Roxisveryhandsome <132827819+Roxisveryhandsome@users.noreply.github.com>
2025-06-29 19:19:20 +0800

    Update day_06.cpp

--- day_06.cpp
+++ day_06.cpp
@@ -9,33 +9,3 @@ void prefix_matrix_sum(int** matrix,int** sum_matrix,int n,int m){
      }
 }
 
-int main(){
-    int n,m,q;
-    cin>>n>>m>>q;
-    int**matrix=new int*[n+1];
-    int**sum_matrix=new int*[n+1];
-    for(int i=0;i<n+1;i++){
-        matrix[i]=new int[m+1]();
-        sum_matrix[i]=new int[m+1]();
-        //这个括号必须是空的，只能初始化为0，其余值必须手动进行初始化
-    }
-    
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            cin>>matrix[i][j];
-        }
-    }
-    prefix_matrix_sum(matrix,sum_matrix,n,m);
-    
-    int x1,y1,x2,y2;
-    int *q_val=new int[q];
-    
-    for(int i=0;i<q;i++){
-        cin>>x1>>y1>>x2>>y2;
-        q_val[i]=sum_matrix[x2][y2]-sum_matrix[x2][y1-1]-sum_matrix[x1-1][y2]+sum_matrix[x1-1][y1-1];
-    }
-    for(int i=0;i<q;i++){
-        cout<<q_val[i]<<endl;
-    }
-    return 0;
-}
